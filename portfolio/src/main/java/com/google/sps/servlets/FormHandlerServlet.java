23361a636a5360f7d2373d2e1e2f3d77b3263c58
Ritvik Banakar <ritvikbanakar15@gmail.com>
2022-06-14 20:30:38 +0000

    Add Sentiment Analysis to Contact Form

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,5 +1,7 @@
 package com.google.sps.servlets;
-
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +17,13 @@ public class FormHandlerServlet extends HttpServlet {
     // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
 
+    Document doc =
+    Document.newBuilder().setContent(textValue).setType(Document.Type.PLAIN_TEXT).build();
+LanguageServiceClient languageService = LanguageServiceClient.create();
+Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+float score = sentiment.getScore();
+languageService.close();
+
     // Print the value so you can see it in the server logs.
     if (textValue.length() != 0) {
         System.out.println("You submitted: " + textValue);
@@ -24,6 +33,11 @@ public class FormHandlerServlet extends HttpServlet {
     response.getWriter().println("<head><title>Submitted</title></head>");
     response.getWriter().println("<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple-v1.css\">");
     response.getWriter().println("<h1>I got your message!</h1>");
+    if (score < 0) {
+        response.getWriter().println("<p>Your input is always appreciated.</p>");
+    } else {
+        response.getWriter().println("<p>Have a great rest of your day!</p>");
+    }
     response.getWriter().println("<a href=\"../\">Click here to go back</a>");
   }
 }
